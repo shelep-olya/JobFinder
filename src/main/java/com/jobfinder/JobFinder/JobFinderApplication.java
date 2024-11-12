3b25ddde69f279bc70e71b0c230d7998c8392041
shelep-olya <shelep.olya@gmail.com>
2024-11-12 12:29:26 +0200

    initialized project; added JobController

---
+++ src/main/java/com/jobfinder/JobFinder/JobFinderApplication.java
@@ -0,0 +1,13 @@
+package com.jobfinder.JobFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFinderApplication.class, args);
+	}
+
+}
